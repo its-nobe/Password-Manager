3cd7052d3d63e88c94297f700094a231c28e4441
its-nobe <aabhay_rana_@outlook.com>
2021-10-02 00:40:24 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import { useState } from "react";
+import Axios from 'axios'
+
+function App() {
+
+  const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+
+  const addPassword = () => {
+    Axios.post("http://localhost:3001/addpassword", {
+      password: password,
+      title: title,
+    });
+  };
+
+  return (
+  <div className="App">
+    <div className="AddingPassword">
+      <input type="text" placeholder="Ex. password123" onChange={(event) => {setPassword(event.target.value);}} />
+      <input type="text" placeholder="Ex. snapchat" onChange={(event) => {setTitle(event.target.value);}} />
+      <button onClick={addPassword}>Add Password</button>
+    </div>
+
+  </div>
+  );
+
+ 
+}
+
+export default App;
